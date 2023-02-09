5c985115f21d73b928326791f7914e83714482df
Alexander Matheson <alexander.matheson@stu.ocsb.ca>
2023-02-09 19:38:14 +0000

    Added MathOperators

---
+++ MathOperators.swift
@@ -0,0 +1,26 @@
+// MathOperators.swift
+//
+//  Created by Alexander Matheson
+//  Created on 2023-Feb-9
+//  Version 1.0
+//  Copyright (c) 2023 Alexander Matheson. All rights reserved.
+//
+//  This program prints two messages.
+import Foundation
+
+//Addition
+print("3 + 4 =", 3 + 4)
+//Subtraction
+print("9 - 4 =", 9 - 4)
+//Multiplication
+print("2 * 3 =", 2 * 3)
+//Division
+print("6/2 =", 6 / 2)
+//Division 2
+print("1/3 =", 1.0 / 3.0)
+//Power
+print("2^2 =", pow(Double(2), 2))
+//Power 2
+print("2^3 =", pow(Double(2), 3))
+//Sqrt
+print("sqrt(9) =", sqrt(9))
